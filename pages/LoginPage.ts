85fd5fd7e4150ae0a5fa4f21197ad6977fb92613
Your Name <you@example.com>
2025-09-13 02:34:57 +0545

    inital setup with stable code

---
+++ pages/LoginPage.ts
@@ -0,0 +1,23 @@
+import { Locator, Page } from 'playwright';
+
+export class LoginPage {
+   readonly page: Page
+   readonly userNameInput:Locator
+   readonly passwordInput:Locator
+   readonly loginButton:Locator
+   constructor(page: Page) {
+      this.page = page;
+      this.userNameInput = page.getByRole('textbox', { name: 'Username' })
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' })
+    this.loginButton = page.getByRole('button', { name: 'Login' })
+   }
+
+  async goToOrangeHRM() {
+      await this.page.goto(process.env.BASE_URL!);
+   }
+   async loginHRM(username: string, password: string) {
+      await this.userNameInput.fill(username);
+      await this.passwordInput.fill(password);
+      await this.loginButton.click();
+   }
+}
---
+++ utils/CommonUtils.ts
@@ -0,0 +1,42 @@
+import cryptoJs from "crypto-js";
+
+export default class CommonUtils {
+  private secretKey: string;
+
+  /**
+   * Initialize secrate key from environment variable
+   */
+  constructor() {
+    if (process.env.secretKey) {
+      this.secretKey = process.env.secretKey;
+    } else {
+      throw new Error("Secret key not found in environment variables");
+    }
+  }
+
+  /**
+   * Provide data from string and it will return encrypted data
+   * @param data
+   * @returns
+   */
+  public encryptData(data: string) {
+    const enccryptedData = cryptoJs.AES.encrypt(
+      data,
+      this.secretKey
+    ).toString();
+    console.log(enccryptedData.toString());
+    return enccryptedData.toString();
+  }
+
+  /**
+   * Provide decrypted data in string format
+   * @param data
+   * @returns
+   */
+  public decryptData(data: string) {
+    const decryptData = cryptoJs.AES.decrypt(data, this.secretKey).toString(
+      cryptoJs.enc.Utf8
+    );
+    return decryptData;
+  }
+}
